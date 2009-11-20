22ad2c9c47f3629e6ef5d95adda48586466197c9
bosie <bosie@noname.(none)>
2009-11-20 15:05:00 +0100

    new ex

--- abgabe1/ex.m
+++ abgabe1/ex.m
@@ -9,18 +9,18 @@ percRun(2);
 display('Aufgabe 1.2.1');
 lms(5, 0.012, 0, 300, 1);
 
-gammas = [0.001:0.002:0.013]
-sserrors = zeros(length(gammas),1)
+gammas = [0.001:0.002:0.013];
+sserrors = zeros(length(gammas),1);
 for(i=1:length(gammas))
     [w, sser, epochs_needed] = lms(5+i, gammas(i), 0, 500, 0);
     sserrors(i) = sser;
 end
 
-sserrors
+sserrors;
 
-gammas = [0.001:0.002:0.013]
-sserrors = zeros(length(gammas),1)
-epochs = zeros(length(gammas),1)
+gammas = [0.001:0.002:0.013];
+sserrors = zeros(length(gammas),1);
+epochs = zeros(length(gammas),1);
 for(i=1:length(gammas))
     [w, sser, epochs_needed] = lms(5, gammas(i), 0.0001, 1000, 0);
     sserrors(i) = sser;
