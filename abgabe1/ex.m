b7e5ded3ebfd53a4155ef6e0a4f595be0afb1021
bosie <bosie@noname.(none)>
2009-11-26 10:41:36 +0100

    figure number bug fixed

--- abgabe1/ex.m
+++ abgabe1/ex.m
@@ -7,12 +7,12 @@ display('Aufgabe 1.1.2 and Aufgabe 1.1.3');
 percRun(2);
 
 display('Aufgabe 1.2.1');
-lms(5, 0.012, 0, 300, 1);
+lms(15, 0.012, 0, 300, 1);
 
 gammas = [0.001:0.002:0.013];
 sserrors = zeros(length(gammas),1);
 for(i=1:length(gammas))
-    [w, sser, epochs_needed] = lms(5+i, gammas(i), 0, 500, 0);
+    [w, sser, epochs_needed] = lms(15+i, gammas(i), 0, 500, 0);
     sserrors(i) = sser;
 end
 
@@ -22,7 +22,7 @@ gammas = [0.001:0.002:0.013];
 sserrors = zeros(length(gammas),1);
 epochs = zeros(length(gammas),1);
 for(i=1:length(gammas))
-    [w, sser, epochs_needed] = lms(5, gammas(i), 0.0001, 1000, 0);
+    [w, sser, epochs_needed] = lms(15, gammas(i), 0.0001, 1000, 0);
     sserrors(i) = sser;
     epochs(i) = epochs_needed;
 end
