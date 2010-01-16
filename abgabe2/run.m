8a7e06925c79ed1717ea4293e1d683d173cfbc43
bosie <bosie@noname.(none)>
2010-01-16 18:19:00 +0100

    first preview

--- abgabe2/run.m
+++ abgabe2/run.m
@@ -1,6 +1,26 @@
 function [ ] = run( )
 
-    [x, target] = genData(10,1);
+    clc;
+    num = 10;
+    [x, target] = genData(num,2);
+    
+    target = target';
+    w = zeros(size(x,2),1);
+    
+    xn = x.* (target*[1 1]); 
+    k= xn*xn'; 
+    gamma =1;
+f= -ones(num,1); 
+Aeq = target'; 
+beq = 0;
+A =zeros(1,num);
+b = 0;
+lb = [zeros(num,1)]; 
+ub = [gamma*ones(num,1)]; 
+[alpha,fvala] = quadprog(k,f,A,b,Aeq,beq,lb,ub);
+alpha
+
+fvala
 
 end
 
