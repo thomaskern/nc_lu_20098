8d1ef10935f3e23ba28a10586b2ecc902b925f8f
unknown <Christian@.(none)>
2010-01-21 12:13:14 +0100

    paint added

---
+++ abgabe2/paintTraining.m
@@ -0,0 +1,31 @@
+function [ ] = paintTraining( data, target,w, figure_id, figure_name )
+
+fig = figure(figure_id);
+set(fig, 'Name', figure_name);
+
+clf();
+
+
+hold on;
+
+for j = 1:length(data)
+    
+    if target(j) == 1
+        plot(data(j,2), data(j,3),'ro');
+    else
+        plot(data(j,2), data(j,3),'g+');
+    end
+end
+grid;
+axis equal;
+
+axis([(min(data(:,2)) - 1) (max(data(:,2)) + 1) (min(data(:,3)) - 1) (max(data(:,3)) + 1)]);
+xaxis = [(min(data(:,2)) - 2) (max(data(:,2)) + 2)];
+hyperplane = (-w(1) - w(2) .* xaxis) / w(3);
+line(xaxis,hyperplane)
+
+hold off;
+drawnow(); % flushes event queque
+
+end
+
