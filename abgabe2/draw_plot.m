ea888b006dcabdefc0310e7ad5636d06841c976d
bosie <bosie@noname.(none)>
2010-01-21 11:07:51 +0100

    merged

--- abgabe2/draw_plot.m
+++ abgabe2/draw_plot.m
@@ -1,12 +1,12 @@
-function [ ] = draw_plot( data, target, alpha, w )
-
+function [ ] = draw_plot( data, target, alpha )
+ 
 fig = figure;
 set(fig, 'Name', 'Nice');
-
+ 
 clf();
-
+ 
 hold on;
-
+ 
 for j = 1:length(data)
     color = 'ro';
     if target(j) < 1
@@ -21,16 +21,15 @@ for j = 1:length(data)
 end
 grid;
 axis equal;
-
-% axis([(min(data(:,1)) - 1) (max(data(:,1)) + 1) (min(data(:,2)) - 1) (max(data(:,2)) + 1)]);
+ 
+% axis([(min(data(:,2)) - 1) (max(data(:,2)) + 1) (min(data(:,3)) - 1) (max(data(:,3)) + 1)]);
 % xaxis = [(min(data(:,2)) - 2) (max(data(:,2)) + 2)];
 % hyperplane = (-w(1) - w(2) .* xaxis) / w(3);
 % line(xaxis,hyperplane)
-
+ 
 hold off;
 drawnow(); % flushes event queque
-
-
-
+ 
+ 
+ 
 end
-
